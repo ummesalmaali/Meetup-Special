b41397784a42b847bfd3019637d726329a548e77
Umme Salma <ummes3527@gmail.com>
2021-10-13 11:58:24 -0700

    initial-commit

---
+++ src/pages/NewMeetUps.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
+const NewMeetUps = () => {
+  return (
+    <section>
+      <h1>Add New Meet Up</h1>
+      <NewMeetupForm />
+    </section>
+  )
+}
+
+export default NewMeetUps
